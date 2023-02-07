fd6c0b6fee68df908abf243f1b41c8ea0904a883
alvjimen <alvjimen@student.42madrid.com>
2023-02-07 20:16:48 +0100

    let me out

--- src/test/test.c
+++ src/test/test.c
@@ -6,49 +6,23 @@
 /*   By: alvjimen <alvjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 20:04:15 by alvjimen          #+#    #+#             */
-/*   Updated: 2023/01/31 20:52:00 by alvjimen         ###   ########.fr       */
+/*   Updated: 2023/02/07 20:16:02 by alvjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "lxr.h"
 #include <stdio.h>
+#include <readline.h>
 
 int	main(int argc, char *argv[])
 {
-	t_tkn	*tkn;
+	char	*str;
 	t_lxr	*lxr;
 
 	if (argc == 1)
 		return (1);
-	lxr = init_lxr(argv[1]);
-	if (!lxr)
-		return (2);
-	tkn = ft_get_token(lxr);
-	if (!tkn)
+	while (1)
 	{
-		free(lxr);
-		return (3);
+		str = readline("> ");
+		free(str);
 	}
-	printf("the command is: %s\n", tkn->value);
-	free(tkn->value);
-	free(tkn);
-	tkn = ft_get_token(lxr);
-	if (!tkn)
-	{
-		free(lxr);
-		return (3);
-	}
-	printf("the arg is: %s\n", tkn->value);
-	free(tkn->value);
-	free(tkn);
-	tkn = ft_get_tkn_dquotes(lxr);
-	if (!tkn)
-	{
-		free(lxr);
-		return (3);
-	}
-	printf("the double quote is: %s\n", tkn->value);
-	free(tkn->value);
-	free(tkn);
-	free(lxr);
-	return (0);
 }
