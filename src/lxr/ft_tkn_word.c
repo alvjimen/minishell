ad2cb25340a86958d40fc1047cc4ae0bb7be3d91
alvjimen <alvjimen@student.42madrid.com>
2023-03-07 15:51:11 +0100

    Taking words and avoid crash

--- src/lxr/ft_tkn_word.c
+++ src/lxr/ft_tkn_word.c
@@ -6,7 +6,7 @@
 /*   By: alvjimen <alvjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 12:54:12 by alvjimen          #+#    #+#             */
-/*   Updated: 2023/03/07 12:07:56 by alvjimen         ###   ########.fr       */
+/*   Updated: 2023/03/07 15:50:21 by alvjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "lxr.h"
@@ -20,14 +20,13 @@ int	ft_token_word(t_lxr	*lxr, size_t *counter)
 	lxr->pos = ft_run_ifs(lxr);
 	while (lxr->str[lxr->pos + *counter] != '\0'
 		&& !ft_char_ifs(lxr->str[lxr->pos + *counter])
-		&& !ft_char_operator(lxr->str[lxr->pos + *counter]))
+		&& !ft_char_operator(lxr, counter))
 	{
-		if (ft_char_quotes(lxr->str[lxr->pos + *counter]))
+		if (ft_char_quotes(lxr->str))
 			ft_quotes(lxr, counter);
-		else
-			counter[0]++;
+		counter[0]++;
 	}
-	if (ft_char_operator(lxr->str[lxr->pos + *counter]))
+	if (ft_char_operator(lxr, counter))
 	{
 		lxr->tokens.token = WORD;
 		printf("Token %s:\n", "WORD");
@@ -35,6 +34,15 @@ int	ft_token_word(t_lxr	*lxr, size_t *counter)
 		write(1, "\n", 1);
 		lxr->pos += *counter;
 		ft_operators(lxr);
+		return (0);
+	}
+	if (ft_char_ifs(lxr->str[lxr->pos + *counter]))
+	{
+		lxr->tokens.token = WORD;
+		printf("Token %s:\n", "WORD");
+		write(1, &lxr->str[lxr->pos], ++counter[0]);
+		write(1, "\n", 1);
+		lxr->pos += *counter;
 		return (0);
 	}
 	return (1);
