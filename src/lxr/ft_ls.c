9ef1ef4e3571745a1c3f5c5f797cd95b058c9549
alvjimen <alvjimen@student.42madrid.com>
2023-04-19 20:23:30 +0200

    Norminette ls

--- src/lxr/ft_ls.c
+++ src/lxr/ft_ls.c
@@ -6,15 +6,73 @@
 /*   By: alvjimen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:35:03 by alvjimen          #+#    #+#             */
-/*   Updated: 2023/04/07 22:01:20 by alvjimen         ###   ########.fr       */
+/*   Updated: 2023/04/19 20:22:53 by alvjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "lxr.h"
 /*
-	if (ft_strncmp(file->d_name, ".", 2) == 0 ||
-	   ft_strncmp(file->d_name, "..", 3) == 0)
-		   continue ;
- */
+	ft_loop
+*/
+char	**ft_close_ls(DIR *dir)
+{
+	if (closedir(dir) == -1)
+		perror("closedir");
+	return (NULL);
+}
+
+char	**ft_error_handle(char **sarr, DIR *dir)
+{
+	ft_sarrfree(&sarr);
+	perror("readdir");
+	return (ft_close_ls(dir));
+}
+
+char	**ft_add_slash_dir(struct dirent *file, char **files, DIR *dir)
+{
+	char	*str;
+
+	str = NULL;
+	if (file && file->d_type == 0x4)
+	{
+		str = ft_strjoin(file->d_name, "/");
+		if (!str)
+		{
+			ft_sarrfree(&files);
+			return (ft_close_ls(dir));
+		}
+		files = ft_sarradd(files, str);
+		if (!files)
+		{
+			ft_sarrfree(&files);
+			free(str);
+			return (ft_close_ls(dir));
+		}
+		free(str);
+	}
+	return (files);
+}
+
+char	**ft_loop(DIR	*dir, struct dirent *file)
+{
+	char	**files;
+
+	files = NULL;
+	while (file)
+	{
+		file = readdir(dir);
+		if (!file && !errno)
+			break ;
+		else if (!file)
+			return ((char **)ft_error_handle(files, dir));
+		files = ft_sarradd(files, file->d_name);
+		if (!files)
+			return (ft_close_ls(dir));
+		files = ft_add_slash_dir(file, files, dir);
+		if (!files)
+			return (ft_close_ls(dir));
+	}
+	return (files);
+}
 
 char	**ft_ls(char *str)
 {
@@ -25,62 +83,17 @@ char	**ft_ls(char *str)
 	dir = opendir(str);
 	if (!dir)
 		return (NULL);
-	files = NULL;
 	file = (struct dirent *)1;
-	str = NULL;
 	errno = 0;
-	while (file)
-	{
-		file = readdir(dir);
-		if (!file && !errno)
-			break ;
-		else if (!file)
-		{
-			perror("readdir");
-			if (closedir(dir) == -1)
-				perror("closedir");
-			return (NULL);
-		}
-		files = ft_sarradd(files, file->d_name);
-		if (!files)
-		{
-			ft_sarrfree(&files);
-			if (closedir(dir) == -1)
-				perror("closedir");
-			return (NULL);
-		}
-		if (file->d_type == 0x4)
-		{
-			str = ft_strjoin(file->d_name, "/");
-			if (!str)
-			{
-				ft_sarrfree(&files);
-				return (NULL);
-			}
-			files = ft_sarradd(files, str);
-			if (!files)
-			{
-				ft_sarrfree(&files);
-				free(str);
-				if (closedir(dir) == -1)
-					perror("closedir");
-				return (NULL);
-			}
-			free(str);
-			str = NULL;
-		}
-	}
+	files = ft_loop(dir, file);
+	if (!files)
+		return (NULL);
 	if (closedir(dir) == -1)
 	{
+		ft_sarrfree(&files);
 		perror("closedir");
 		return (NULL);
 	}
-	free(str);
 	ft_sarrsort(files);
-	/*
-	ft_sarrprint(files);
-	 * This is for avoid leaks i should remove it the same with the return value
-	ft_sarrfree(&files);
-	 */
 	return (files);
 }
