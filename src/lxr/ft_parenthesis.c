415edd5fa4d5225b6f6b2c8e31c8aba98671916f
alvjimen <alvjimen@student.42madrid.com>
2023-10-20 16:02:01 +0200

    Update ft_parenthesis i create bug for fix one

--- src/lxr/ft_parenthesis.c
+++ src/lxr/ft_parenthesis.c
@@ -6,7 +6,7 @@
 /*   By: alvjimen <alvjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 17:53:50 by alvjimen          #+#    #+#             */
-/*   Updated: 2023/10/20 12:31:54 by alvjimen         ###   ########.fr       */
+/*   Updated: 2023/10/20 16:00:11 by alvjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "lxr.h"
@@ -30,9 +30,9 @@ static int	ft_search_close_parenthesis(t_lxr *lxr, size_t *counter,
 			return (FAILURE);
 		counter[0]++;
 	}
+	lxr->tokens.states ^= PAREN;
 	if (counter_par[0] > 0)
 		return (FAILURE);
-	lxr->tokens.states ^= PAREN;
 	return (SUCCESS);
 }
 
@@ -50,20 +50,20 @@ int	ft_parenthesis(t_lxr *lxr)
 	size_t	counter_par;
 
 	counter = 0;
-	counter_par = 0;
 	if (lxr->str[lxr->pos] != '(')
 		return (SUCCESS);
+	counter_par = 1;
 	counter++;
 	lxr->tokens.token = PARENTHESIS;
 	lxr->tokens.operators = NONE;
 	lxr->tokens.states = PAREN;
-	while (lxr->tokens.states == PAREN)
-	{
-		if (ft_search_close_parenthesis(lxr, &counter, &counter_par) == FAILURE)
-			return (FAILURE);
-		if (lxr->str[lxr->pos + counter] == ')')
-			return (ft_set_token(lxr, counter));
-	}
+	if (ft_search_close_parenthesis(lxr, &counter, &counter_par) == FAILURE)
+		return (FAILURE);
+	if (lxr->str[lxr->pos + counter - 1] == ')')
+		return (ft_set_token(lxr, counter));
+	//ft_set_token(lxr, counter)
+	//((t_tkns *)lxr->btree->content)->;
+	lxr->pos += counter;
 	ft_putstr_fd("NOT close parenthesis\n", 2);
-	return (NOT_TOKEN);
+	return (FAILURE);
 }
